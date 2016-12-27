fbe216d36248b0f0a6ac1c0dfc7fcfcc78410944
Karl Horky <karl.horky@gmail.com>
2016-12-27 22:06:02 +0100

    Configure babel to ignore example-review-module

--- packages/react-server-cli/src/cli.js
+++ packages/react-server-cli/src/cli.js
@@ -4,6 +4,6 @@ const cli = require(".");
 
 cli.parseCliArgs().then(args => {
   const config = args.rulesPath ? JSON.parse(fs.readFileSync(path.resolve(args.rulesPath + '/.babelrc'))) : {};
-  require("babel-core/register")(config);
+  require("babel-core/register")(Object.assign(config, { ignore: /node_modules\/(?!example-review-module)/ }));
   cli.run(args);
 }).catch(console.error);
